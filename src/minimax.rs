f69600bd9161d2a3f45c642f01941abed769d0cc
Joe Chan <joseph6.626@gmail.com>
2023-09-30 23:51:38 +1000

    second minimax unit test

--- src/minimax.rs
+++ src/minimax.rs
@@ -4,12 +4,24 @@ mod test_minimax {
 
     #[test]
     fn test_terminal_state() {
-        let node = (1, String::from(""));
+        let node = (0, 1, String::from(""));
         let tree = vec![node.clone()];
         assert_eq!(minimax(node.clone(), tree, true), 1)
     }
+
+    #[test]
+    fn test_two_steps() {
+        let node1 = (0, 1, String::from("1"));
+        let node2 = (1, 1, String::from(""));
+        let tree = vec![node1.clone(), node2.clone()];
+        assert_eq!(minimax(node1.clone(), tree, true), -1)
+    }
 }
 
-pub fn minimax(node: (i32, String), tree:Vec<(i32, String)>, maximize: bool) -> i32 {
-    node.0
+pub fn minimax(node: (u32, i32, String), tree:Vec<(u32, i32, String)>, maximize: bool) -> i32 {
+    if node.2.len() == 0 {
+        return node.1
+    } else {
+        return -1
+    }
 }
