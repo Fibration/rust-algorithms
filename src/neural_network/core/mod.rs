cdcc26d004871755e090a22f58ca272df1eab546
Joe Chan <joseph6.626@gmail.com>
2023-11-10 19:23:50 +1100

    He initialisation

--- src/neural_network/core/mod.rs
+++ src/neural_network/core/mod.rs
@@ -2,7 +2,9 @@ pub mod array;
 pub mod function;
 mod test;
 pub mod traits;
+pub mod utilities;
 
 pub use self::array::*;
 pub use self::function::*;
 pub use self::traits::*;
+pub use self::utilities::*;
---
+++ src/neural_network/core/utilities.rs
@@ -0,0 +1,14 @@
+use rand::thread_rng;
+use rand_distr::{Distribution, Normal};
+
+pub fn he_initialise(dim_in: usize, dim_out: usize) -> Vec<f64> {
+    let factor = (2.0 / (dim_in as f64)).sqrt();
+    let mut rng = thread_rng();
+    let normal = Normal::new(0.0, factor).unwrap();
+
+    (0..dim_out)
+        .collect::<Vec<_>>()
+        .iter()
+        .map(|_| normal.sample(&mut rng))
+        .collect()
+}
