3401924db6c6db600b0f01ae0bc1edaee3f33649
Joe Chan <joseph6.626@gmail.com>
2023-12-02 19:24:47 +1100

    back() for new conv2d

--- src/neural_network/convolutional/conv2d.rs
+++ src/neural_network/convolutional/conv2d.rs
@@ -39,7 +39,61 @@ impl Layer2D for Conv2D {
         Option<Vec<Vec<Vec<f32>>>>,
         Option<Vec<f32>>,
     ) {
-        todo!()
+        // TODO adjust error
+        // TODO rotate filter in error_by_input
+        // TODO pad for full convolution in filter_error
+        let error_by_input = input
+            .iter()
+            .map(|i| {
+                self.filters
+                    .iter()
+                    // get contribution of each input to the error by
+                    // calculating the convolution of the input by the filter
+                    // then taking the entrywise product with the relevant error;
+                    // this is then convolved with the filter to get the input_error
+                    // then summed entrywise for that input
+                    .zip(error.iter())
+                    .map(|(filter, filter_error)| {
+                        matrix_op(
+                            &convolution(i, filter, self.padding, self.stride),
+                            filter_error,
+                            |x, y| x * y,
+                        )
+                    })
+                    .zip(self.filters.iter())
+                    .map(|(error_channel, filter)| {
+                        convolution(&error_channel, filter, (0, 0), (1, 1))
+                    })
+                    .reduce(|acc, err| matrix_op(&acc, &err, |x, y| x + y))
+                    .unwrap()
+            })
+            .collect();
+
+        let filter_error = self
+            .filters
+            .iter()
+            .zip(error.iter())
+            .map(|(filter, filter_error)| {
+                input
+                    .iter()
+                    .map(|input_piece| {
+                        convolution(
+                            input_piece,
+                            &matrix_op(
+                                &convolution(input_piece, filter, self.padding, self.stride),
+                                &filter_error,
+                                |x, y| x * y,
+                            ),
+                            self.padding,
+                            (1, 1),
+                        )
+                    })
+                    .reduce(|acc, x| matrix_op(&acc, &x, |y, z| y + z))
+                    .unwrap()
+            })
+            .collect();
+
+        (error_by_input, Some(filter_error), None)
     }
 }
 
@@ -47,5 +101,12 @@ fn matrix_sum(a: &[Vec<f32>], b: &[Vec<f32>]) -> Vec<Vec<f32>> {
     a.iter()
         .zip(b.iter())
         .map(|(x, y)| x.iter().zip(y.iter()).map(|(j, k)| j + k).collect())
+        .collect()
+}
+
+fn matrix_op(a: &[Vec<f32>], b: &[Vec<f32>], func: fn(&f32, &f32) -> f32) -> Vec<Vec<f32>> {
+    a.iter()
+        .zip(b.iter())
+        .map(|(x, y)| x.iter().zip(y.iter()).map(|(j, k)| func(j, k)).collect())
         .collect()
 }
